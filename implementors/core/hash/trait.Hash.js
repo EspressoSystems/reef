56debe73977ee956b8b41f5564cbefbe82495cc8
Ancient123 <Ancient123@users.noreply.github.com>
2022-07-25 00:31:47 +0000

    deploy: 7b45d90bcf1894a4a542e1680ccdf8cb7e6df247

--- implementors/core/hash/trait.Hash.js
+++ implementors/core/hash/trait.Hash.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["reef"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.62.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"reef/cap/enum.TransactionKind.html\" title=\"enum reef::cap::TransactionKind\">TransactionKind</a>","synthetic":false,"types":["reef::cap::TransactionKind"]}];
+implementors["reef"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.62.1/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"reef/cap/enum.TransactionKind.html\" title=\"enum reef::cap::TransactionKind\">TransactionKind</a>","synthetic":false,"types":["reef::cap::TransactionKind"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
