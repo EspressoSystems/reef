d2ef1da8d85f8c9ce612c3602235f4af4747580b
Ancient123 <Ancient123@users.noreply.github.com>
2023-05-01 00:23:04 +0000

    deploy: 37ec6746223820aeb21978f6c45dd3ea77d463f0

--- reef/sidebar-items.js
+++ reef/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":[["cap","Minimal implementation of the ledger traits for a CAP-style ledger."],["traits","Traits describing the interface of a CAP ledger."],["types","Ledger types"]]};
+window.SIDEBAR_ITEMS = {"mod":["cap","traits","types"]};
