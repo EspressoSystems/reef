d0739d7285a275112cd27036028f73d30c062ff5
Ancient123 <Ancient123@users.noreply.github.com>
2023-08-01 00:27:24 +0000

    deploy: 3e61025e90cdd0bf5337f23772c9ebc8eff0d8d9

--- implementors/arbitrary/trait.Arbitrary.js
+++ implementors/arbitrary/trait.Arbitrary.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"reef":[["impl&lt;'a, const H: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.70.0/std/primitive.u8.html\">u8</a>&gt; Arbitrary&lt;'a&gt; for <a class=\"struct\" href=\"reef/cap/struct.Validator.html\" title=\"struct reef::cap::Validator\">Validator</a>&lt;H&gt;"]]
+"reef":[["impl&lt;'a, const H: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.71.0/std/primitive.u8.html\">u8</a>&gt; Arbitrary&lt;'a&gt; for <a class=\"struct\" href=\"reef/cap/struct.Validator.html\" title=\"struct reef::cap::Validator\">Validator</a>&lt;H&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
