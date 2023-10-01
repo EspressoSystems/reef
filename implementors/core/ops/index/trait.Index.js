8900820791397babefbb607b0a146c4cf3c1f427
Ancient123 <Ancient123@users.noreply.github.com>
2023-10-01 00:23:04 +0000

    deploy: 3e61025e90cdd0bf5337f23772c9ebc8eff0d8d9

--- implementors/core/ops/index/trait.Index.js
+++ implementors/core/ops/index/trait.Index.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"reef":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.1/core/ops/index/trait.Index.html\" title=\"trait core::ops::index::Index\">Index</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.71.1/std/primitive.usize.html\">usize</a>&gt; for <a class=\"struct\" href=\"reef/cap/struct.Block.html\" title=\"struct reef::cap::Block\">Block</a>"]]
+"reef":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.72.1/core/ops/index/trait.Index.html\" title=\"trait core::ops::index::Index\">Index</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.72.1/std/primitive.usize.html\">usize</a>&gt; for <a class=\"struct\" href=\"reef/cap/struct.Block.html\" title=\"struct reef::cap::Block\">Block</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
