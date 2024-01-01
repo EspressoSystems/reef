f36cb05406633bc7252149daf28bd347ba9cebfe
Ancient123 <Ancient123@users.noreply.github.com>
2024-01-01 00:22:09 +0000

    deploy: 3e61025e90cdd0bf5337f23772c9ebc8eff0d8d9

--- implementors/core/default/trait.Default.js
+++ implementors/core/default/trait.Default.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"reef":[["impl&lt;const H: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.74.0/std/primitive.u8.html\">u8</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"reef/cap/struct.Validator.html\" title=\"struct reef::cap::Validator\">Validator</a>&lt;H&gt;"]]
+"reef":[["impl&lt;const H: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.74.1/std/primitive.u8.html\">u8</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.1/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"reef/cap/struct.Validator.html\" title=\"struct reef::cap::Validator\">Validator</a>&lt;H&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
